369661a0ab7d566ec8972f2c40d925cc3536fd7e
dongfl <1075608781@qq.com>
2020-07-10 10:06:54 +0800

    smsdemo

---
+++ src/test/java/com/dongfl/smsdemo/SmsdemoApplicationTests.java
@@ -0,0 +1,45 @@
+package com.dongfl.smsdemo;
+
+import com.aliyuncs.CommonRequest;
+import com.aliyuncs.CommonResponse;
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.exceptions.ServerException;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.profile.DefaultProfile;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+        DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "", "");
+        IAcsClient client = new DefaultAcsClient(profile);
+
+        CommonRequest request = new CommonRequest();
+        request.setSysMethod(MethodType.POST);
+        request.setSysDomain("dysmsapi.aliyuncs.com");
+        request.setSysVersion("2017-05-25");
+        request.setSysAction("SendSms");
+        request.putQueryParameter("RegionId", "cn-hangzhou");
+
+        request.putQueryParameter("PhoneNumbers","17717856686");
+        request.putQueryParameter("SignName","O家云协作平台");
+        request.putQueryParameter("TemplateCode","SMS_195871107");
+        request.putQueryParameter("TemplateParam", "{\"code\":"+1234+"}");
+
+        try {
+            CommonResponse response = client.getCommonResponse(request);
+            System.out.println(response.getData());
+        } catch (ServerException e) {
+            e.printStackTrace();
+        } catch (ClientException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
